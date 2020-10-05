959c202fcb5f6fab0195c7a67c51ddea47d74f1b
kchafla <kchaflam@fp.insjoaquimmir.cat>
2020-10-05 19:31:22 +0200

    Calendari millor

--- UF1/Activitat2/UF1-A2-Practica.php
+++ UF1/Activitat2/UF1-A2-Practica.php
@@ -11,18 +11,18 @@
 			$mes = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 			$mesactual = date("F");
 			$espai = 1;
+			echo "<table style='background-color:#fdedec;' border='1'>";
 			if ( $mesactual == $mes[$m] ) {
-				echo "<table style='background-color:#fadbd8;' border='1'>";
+				echo "<tr><th style='color:red;'colspan='7'>$mes[$m]</th></tr>";
 			} else {
-				echo "<table style='background-color:#fdf2e9;' border='1'>";
+				echo "<tr><th colspan='7'>$mes[$m]</th></tr>";
 			}
-			echo "<tr><th colspan='7'>$mes[$m]</th></tr>";
 			echo "<tr><td>L</td><td>M</td><td>X</td><td>J</td><td>V</td><td>S</td><td>D</td></tr><br><tr>";
 			for ( $dia=1; $dia<=date("N",strtotime("01".$mes[$m].date("Y"))); $dia++ ){
 				if ( $dia == date("N",strtotime("01".$mes[$m].date("Y")))){
 					for ( $dia=1; $dia<=date("t"); $dia++ ){
 						if( $dia == date("j") && $mesactual == $mes[$m]){
-							echo "<td style='color:red'>$dia</td>";
+							echo "<td style='color:red;'>$dia</td>";
 						} else {
 							echo "<td>$dia</td>";
 						}
