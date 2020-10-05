abfd35e52fdc174fcc4008db12051d2dd085feac
kchafla <kchaflam@fp.insjoaquimmir.cat>
2020-10-05 19:13:06 +0200

    Calendari millor

--- UF1/Activitat2/UF1-A2-Practica.php
+++ UF1/Activitat2/UF1-A2-Practica.php
@@ -7,31 +7,37 @@
 </head>
 <body>
 	<?php
-		$mes = date("F");
-		$espai = 1;
-		echo "<table border='1'>";
-		echo "<tr><th colspan='7'>$mes</th></tr>";
-		echo "<tr><td>L</td><td>M</td><td>X</td><td>J</td><td>V</td><td>S</td><td>D</td></tr><br><tr>";
-		for ( $dia=1; $dia<=date("w",strtotime(date("Y").date("n")."01")); $dia++ ){
-			if ( $dia == date("w",strtotime(date("Y").date("n")."01")) ){
-				for ( $dia=1; $dia<=date("t"); $dia++ ){
-					if( $dia == date("j") ){
-						echo "<td style='color:red'>$dia</td>";
-					} else {
-						echo "<td>$dia</td>";
-					}
-					if( ($espai%7) == 0 ){
-						echo "</tr><tr>";
-					}
-					$espai++;
-				}
+		for ( $m=0; $m < 12; $m++) {
+			$mes = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+			$mesactual = date("F");
+			$espai = 1;
+			if ( $mesactual == $mes[$m] ) {
+				echo "<table style='background-color:#fadbd8;' border='1'>";
 			} else {
-				echo "<td></td>";
+				echo "<table style='background-color:#fdf2e9;' border='1'>";
 			}
-			$espai++;
+			echo "<tr><th colspan='7'>$mes[$m]</th></tr>";
+			echo "<tr><td>L</td><td>M</td><td>X</td><td>J</td><td>V</td><td>S</td><td>D</td></tr><br><tr>";
+			for ( $dia=1; $dia<=date("N",strtotime("01".$mes[$m].date("Y"))); $dia++ ){
+				if ( $dia == date("N",strtotime("01".$mes[$m].date("Y")))){
+					for ( $dia=1; $dia<=date("t"); $dia++ ){
+						if( $dia == date("j") && $mesactual == $mes[$m]){
+							echo "<td style='color:red'>$dia</td>";
+						} else {
+							echo "<td>$dia</td>";
+						}
+						if( ($espai%7) == 0 ){
+							echo "</tr><tr>";
+						}
+						$espai++;
+					}
+				} else {
+					echo "<td></td>";
+				}
+				$espai++;
+			}
+			echo "</table>";
 		}
-		echo "</table>";
-		echo "me parece bien";
 	?>
 </body>
 </html>
