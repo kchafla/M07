f74be03eb81e0a7fa487077e5670bfe8aefc3eb9
kchafla <kchaflam@fp.insjoaquimmir.cat>
2020-10-21 17:22:41 +0200

    Lo de aceptar cookies

--- UF1/Activitat 4/UF1-A4-ExerciciPrivat.php
+++ UF1/Activitat 4/UF1-A4-ExerciciPrivat.php
@@ -1,6 +1,15 @@
 <?php
     session_start();
-
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
     if($_SERVER["REQUEST_METHOD"] == "POST") {
         session_destroy();
         header("Location: UF1-A4-ExerciciPublic.php");
@@ -10,24 +19,10 @@
         } else {
             header("Location: UF1-A4-ExerciciPublic.php");
         }
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <form method="post">
-            <button type="submit">Salir</button>
-        </form>
-    </body>
-</html>
-
-<?php
-
     }
-
 ?>
+<form method="post">
+    <button type="submit">Salir</button>
+</form>
+</body>
+</html>
--- UF1/Activitat 4/UF1-A4-ExerciciPublic.php
+++ UF1/Activitat 4/UF1-A4-ExerciciPublic.php
@@ -1,31 +1,53 @@
 <?php
     session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
     include "../funcions.php";
-
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (comprovar_email($_REQUEST["user"]) && comprovar_contra($_REQUEST["password"])) {
-            $_SESSION["user"] = comprovar_campo($_REQUEST["user"]);
-            $_SESSION["password"] = comprovar_campo($_REQUEST["password"]);
-            header("Location: UF1-A4-ExerciciPrivat.php");
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (isset($_REQUEST["aceptar"])) {
+            if ($_REQUEST["aceptar"] == "Si") {
+                setcookie("aceptado", 1, time() + 365 * 24 * 60 * 60);
+                header("Location: UF1-A4-ExerciciPublic.php");
+            } else {
+                header("Location: https://www.google.es");
+            }
         } else {
-            echo "<p>Datos incorrectos!</p>";
+            if (comprovar_email($_REQUEST["user"]) && comprovar_contra($_REQUEST["password"])) {
+                $_SESSION["user"] = comprovar_campo($_REQUEST["user"]);
+                $_SESSION["password"] = comprovar_campo($_REQUEST["password"]);
+                header("Location: UF1-A4-ExerciciPrivat.php");
+            } else {
+                echo "<p>Datos incorrectos!</p>";
+            }
         }
     }
 
+    if (isset($_COOKIE["aceptado"])) {
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <form method="post">
-            <label>Email: </label><input type="text" name="user"/><br>
-            <label>Contraseña: </label><input type="password" name="password"/><br>
-            <button type="submit">Iniciar sesión</button>
-        </form>
-    </body>
+<form method="post">
+    <label>Email: </label><input type="text" name="user"/><br>
+    <label>Contraseña: </label><input type="password" name="password"/><br>
+    <label>Recordar contraseña?</label><input type="checkbox" name="recordar" value="1"/><br>
+    <button type="submit">Iniciar sesión</button>
+</form>
+<?php
+    } else {
+?>
+<form method="post">
+    <label>Esta pagina usa cookies, quieres continuar?</label><br>
+    <button type="submit" name="aceptar" value="Si">Si. </button>
+    <button type="submit" name="aceptar" value="No">No.</button>
+</form>
+<?php
+    }
+?>
+</body>
 </html>
