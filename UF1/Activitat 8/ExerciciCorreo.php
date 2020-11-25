938dc87d294a898b1deabd82d9d07162a929082d
kchafla <kchaflam@fp.insjoaquimmir.cat>
2020-11-25 17:34:22 +0100

    Arreglar carpetes

---
+++ UF1/Activitat 8/ExerciciCorreo.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
